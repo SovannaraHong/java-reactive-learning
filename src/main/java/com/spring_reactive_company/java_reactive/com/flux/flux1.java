5673199bf25a75721a859d6776d195d1cb2e22d6
SovannaraHong <rapewpew372@gmail.com>
2026-06-13 20:02:13 +0700

    flux and mono

--- src/main/java/com/spring_reactive_company/java_reactive/com/flux/flux1.java
+++ src/main/java/com/spring_reactive_company/java_reactive/com/flux/flux1.java
@@ -3,4 +3,15 @@ package com.spring_reactive_company.java_reactive.com.flux;
 import reactor.core.publisher.Flux;
 
 public class flux1 {
+    public static void main(String[] args) {
+
+        //flux it can store 0->N
+        //mono only 0->1
+
+        Flux<String> flux = Flux.just("Hello", "World");
+//        System.out.println(flux);
+        flux.subscribe(System.out::println);
+
+    }
+
 }
--- src/main/java/com/spring_reactive_company/java_reactive/com/mono/mono1.java
+++ src/main/java/com/spring_reactive_company/java_reactive/com/mono/mono1.java
@@ -1,4 +1,9 @@
 package com.spring_reactive_company.java_reactive.com.mono;
 
+import reactor.core.publisher.Mono;
+
 public class mono1 {
+    public static void main(String[] args) {
+        Mono<Integer> mono1 = Mono.just(1);
+    }
 }
